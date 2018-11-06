003f104e0c3fbdf785bf02d74a08c64888935cd4
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-11-06 11:52:42 -0500

    GearVR
    shrug

---
+++ GearVR/Assets/ButtonPress.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPress : MonoBehaviour
+{
+    public Text output;
+
+    private int _pressCount = 0;
+
+    private void Start()
+    {
+        UpdateText();
+    }
+
+    public void ButtonDown()
+    {
+        _pressCount++;
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        if(!output) { return; }
+        output.text = "Press count: " + _pressCount;
+    }
+}
