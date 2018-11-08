d840a4efc351117e02963f76af503f7bbc1e0675
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-11-08 11:41:16 -0500

    Controller input menu

---
+++ GearVR/Assets/DisplayInput.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class DisplayInput : MonoBehaviour
+{
+    Text _outputField;
+
+    private void Start()
+    {
+        _outputField = GetComponent<Text>();
+    }
+
+    private void Update ()
+    {
+        _outputField.text = GetOutput();
+	}
+
+    private string GetOutput()
+    {
+        OVRInput.Controller c = OVRInput.GetActiveController();
+        bool isConnected = c != OVRInput.Controller.None;
+        if(isConnected) { isConnected = OVRInput.IsControllerConnected(c); }
+        isConnected = true;
+        string message = "Connected: " + isConnected;
+        if(isConnected)
+        {
+            message += "\nTouchPos: ";
+            if (OVRInput.Get(OVRInput.Touch.PrimaryTouchpad))
+            {
+                Vector2 touchPos = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+                message += "(" + touchPos.x + "," + touchPos.y + ")";
+            }
+            else
+            {
+                message += "N/A";
+            }
+
+            message +=
+                "\nClick: " + OVRInput.Get(OVRInput.Button.PrimaryTouchpad) +
+                "\nApp: " + OVRInput.Get(OVRInput.Button.Back) +
+                "\nTrigger: " + OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger);
+        }
+
+        return message;
+    }
+}
